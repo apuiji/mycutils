4c2cf0ec0224f6e46370af76fe76b5bef0bd6457
apuiji <2330683684@qq.com>
2024-05-09 14:36:47 +0800

    a

--- xyz.h
+++ xyz.h
@@ -3,6 +3,7 @@
 
 #include<stdbool.h>
 #include<stddef.h>
+#include<string.h>
 
 #define zltMemb(p, t, m) (((t *) (p))->m)
 #define zltOffsetOf(t, m) ((size_t) &zltMemb(NULL, t, m))
@@ -18,11 +19,35 @@ typedef struct {
   size_t size;
 } zltString;
 
-static inline zltString zltStringMake(const char *data, size_t size) {
+static inline zltString zltStrMake(const char *data, size_t size) {
   return (zltString) { .data = data, .size = size };
 }
 
 /// @param data requires string constant literal
-#define zltStringMakeStatic(data) zltStringMake(data, sizeof(data))
+#define zltStrMakeStatic(data) zltStringMake(data, sizeof(data))
+
+/// different of stdlib strtol, requires /^\d+/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToLong(long *dest, const char *it, const char *end);
+
+/// different of stdlib strtol, requires /^[01]+/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToLong2(long *dest, const char *it, const char *end);
+
+/// different of stdlib strtol, requires /^[0-3]+/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToLong4(long *dest, const char *it, const char *end);
+
+/// different of stdlib strtol, requires /^[0-7]+/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToLong8(long *dest, const char *it, const char *end);
+
+/// different of stdlib strtol, requires /^[[:xdigit:]]+/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToLong16(long *dest, const char *it, const char *end);
+
+/// different of stdlib strtod, requires /^(\d+\.\d*|\.?\d+)([Ee][+-]?\d+)?/, ignore bads like out of range
+/// @param[out] dest usually initialized by 0
+const char *zltStrToDouble(double *dest, const char *it, const char *end);
 
 #endif
